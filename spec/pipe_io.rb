c83850b695bd21023b141aedc3c9e0192f8747e0
Chris Berkhout <chrisberkhout@gmail.com>
2013-02-10 10:33:41 +1100

    Fillled out io spec a bit. Need to find way to assert that read end is never closed.

--- spec/pipe_io.rb
+++
@@ -1,7 +0,0 @@
-describe "pipe io" do
-  it "should raise an error when..."
-  it "should not raise an error when..."
-  it "should ... when an EOF is written before close"
-  it "should ..."
-end
-
